d73c26516ca41dbbfc6b78b46459b2ba7e1a5e53
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2019-04-17 17:12:53 +0100

    Added player one and two methods to Game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,18 @@
 class Game
+  attr_reader :player_one, :player_two
+
+  def initialize(player_one, player_two)
+    @players= [player_one, player_two]
+  end
+
+  def player_one
+    @player_one = @players.first
+  end
+
+  def player_two
+    @player_two = @players.last
+  end
+
   def attack(player)
     player.receive_damage
   end
