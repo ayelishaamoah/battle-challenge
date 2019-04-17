c68573258829f2061377ea7b98bae58c25af2e83
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2019-04-17 17:12:12 +0100

    Added player one and two tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,14 +1,28 @@
 require 'game'
 
 describe Game do
-  let (:subject) { described_class.new }
+
+  let (:player_one) { double :player_one }
   let (:player_one) { double :player_one }
   let (:player_two) { double :player_two }
+  let (:subject) { described_class.new(player_one, player_two) }
 
   describe '#attack' do
     it 'damages the player' do
       expect(player_two).to receive(:receive_damage)
       subject.attack(player_two)
+    end
+  end
+
+  describe '#player_one' do
+    it 'returns player 1' do
+      expect(subject.player_one).to eq player_one
+    end
+  end
+
+  describe '#player_two' do
+    it 'returns player 2' do
+      expect(subject.player_two).to eq player_two
     end
   end
 end
