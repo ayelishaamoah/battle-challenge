4b8a1a832dd8eab64b5711686ed786ac8fb5b43f
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2019-04-18 14:27:27 +0100

    Updated switch turns feature test scenario description

--- spec/features/switch_turns_spec.rb
+++ spec/features/switch_turns_spec.rb
@@ -1,10 +1,10 @@
 feature 'switching turns' do
-  scenario 'you can see the current turn' do
+  scenario 'you can see the current players turn' do
     sign_in_and_play
     expect(page).to have_content "Ayelisha's turn"
   end
 
-  scenario 'after an attack' do
+  scenario 'after an attack the player switches' do
     sign_in_and_play
     click_button 'Attack'
     click_button 'Play again'
